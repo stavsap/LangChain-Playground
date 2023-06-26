e411fc28afd17b61295a1397b9391778acd5f9f6
Stav Sapir <stavsap@gmail.com>
2023-06-26 22:05:21 +0300

    Create install_linux.sh

---
+++ DocChat/install_linux.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/stavsap/LangChain-Playground.git
+cd LangChain-Playground/DocChat
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install -r requirements.txt
