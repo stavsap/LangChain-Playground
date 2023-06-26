9fc3c87491d1b5ec86073299081a36a8998bced0
Stav Sapir <stavsap@gmail.com>
2023-06-26 22:08:24 +0300

    Delete install.sh

--- DocChat/install.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/stavsap/LangChain-Playground.git
-cd LangChain-Playground/DocChat
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install -r requirements.txt
