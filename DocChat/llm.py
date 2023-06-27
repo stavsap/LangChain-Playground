55de4535567759cf8a788d1497fe05bf413a9d79
Stav Sapir <stavsap@gmail.com>
2023-06-27 17:50:19 +0300

    Update llm.py

--- DocChat/llm.py
+++ DocChat/llm.py
@@ -2,11 +2,18 @@ from langchain.llms import TextGen, HuggingFacePipeline
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 
+from repository import getDB
 
-embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
-llm = TextGen(model_url = "http://localhost:5000")
-qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+qa = None
 
+def setupLLM():
+  db = getDB()
+  if db is None:
+    return
+  llm = TextGen(model_url = "http://localhost:5000")
+  qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=db.as_retriever(), return_source_documents=True)
 
 def query(msg):
+  if qa is None:
+    return "LLM is not set, please set LLM conectivity."
   return msg
