0c196a97a501192048d6770b9c9b26c1b84f02db
Stav Sapir <stavsap@gmail.com>
2023-06-27 20:11:20 +0300

    Update repository.py

--- DocChat/repository.py
+++ DocChat/repository.py
@@ -19,8 +19,6 @@ from settings import EMBEDDING_MODEL_NAME
 
 db = None
 
-embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
-
 def load_single_document(file_path: str) -> Document:
     file_extension = os.path.splitext(file_path)[1]
     loader_class = DOCUMENT_MAP.get(file_extension)
@@ -33,18 +31,13 @@ def load_single_document(file_path: str) -> Document:
 
 def load_document_batch(filepaths):
     logging.info("Loading document batch")
-    # create a thread pool
     with ThreadPoolExecutor(len(filepaths)) as exe:
-        # load files
         futures = [exe.submit(load_single_document, name) for name in filepaths]
-        # collect data
         data_list = [future.result() for future in futures]
-        # return data and file paths
         return (data_list, filepaths)
 
 
 def load_documents(source_dir: str) -> list[Document]:
-    # Loads all documents from the source documents directory
     all_files = os.listdir(source_dir)
     paths = []
     for file_path in all_files:
@@ -53,27 +46,20 @@ def load_documents(source_dir: str) -> list[Document]:
         if file_extension in DOCUMENT_MAP.keys():
             paths.append(source_file_path)
 
-    # Have at least one worker and at most INGEST_THREADS workers
     n_workers = min(INGEST_THREADS, max(len(paths), 1))
     chunksize = round(len(paths) / n_workers)
     docs = []
     with ProcessPoolExecutor(n_workers) as executor:
         futures = []
-        # split the load operations into chunks
         for i in range(0, len(paths), chunksize):
-            # select a chunk of filenames
             filepaths = paths[i : (i + chunksize)]
-            # submit the task
             future = executor.submit(load_document_batch, filepaths)
             futures.append(future)
-        # process all results
         for future in as_completed(futures):
-            # open the file and load the data
             contents, _ = future.result()
             docs.extend(contents)
 
     return docs
-
 
 def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
     # Splits documents for correct Text Splitter
@@ -90,6 +76,7 @@ def split_documents(documents: list[Document]) -> tuple[list[Document], list[Doc
 def provisionDB():
     global db
     if db is None:
+        embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
         db = Chroma(
                 persist_directory=DB_DIR,
                 embedding_function=embeddings,
@@ -109,6 +96,8 @@ def ingest(device_type = "cuda"):
     logging.info(f"Loading documents from {DOCS_DIR}")
     
     documents = load_documents(DOCS_DIR)
+
+    logging.info("Found "+str(len(documents))+" documents")
     
     text_documents, python_documents = split_documents(documents)
     
@@ -125,15 +114,14 @@ def ingest(device_type = "cuda"):
     
     logging.info(f"Split into {len(texts)} chunks of text")
 
-    db.delete_collection()
+    logging.info("Dropping current embeddings")
+
+    db.delete(db.get()["ids"])
+
+    logging.info("Embeddings new documents")
 
     db.add_documents(texts)
-
-    # db = Chroma.from_documents(
-    #     texts,
-    #     embeddings,
-    #     persist_directory=DB_DIR,
-    #     client_settings=CHROMA_SETTINGS,
-    # )
     
     db.persist()
+
+    logging.info("All complete, all embeddings persisted")
