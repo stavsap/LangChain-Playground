b0ebed5de4d3955793a8c5a180a8519d6293be4a
Stav Sapir <stavsap@gmail.com>
2023-06-27 17:40:25 +0300

    Update llm.py

--- DocChat/llm.py
+++ DocChat/llm.py
@@ -1,3 +1,12 @@
+from langchain.llms import TextGen, HuggingFacePipeline
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+
+embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": "cuda"})
+llm = TextGen(model_url = "http://localhost:5000")
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
 
 def query(msg):
   return msg
