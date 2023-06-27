206d79093f5b0889b2869acfc493136e29432f28
Stav Sapir <stavsap@gmail.com>
2023-06-27 18:55:02 +0300

    Update settings.py

--- DocChat/settings.py
+++ DocChat/settings.py
@@ -1,3 +1,15 @@
+import logging
 
 LLM_URL = "http://localhost:5000"
 
+LLM_USERNAME = None
+
+LLM_PASSWORD = None
+
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"
+
+def saveSetting():
+  logging.error("saveSetting NOT IMPLEMENTED")  
+
+def loadSettings():
+  logging.error("saveSetting NOT IMPLEMENTED")  
