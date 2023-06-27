ebbd10006d2b6ff76b6ff8c274e5cadd5739c9dd
Stav Sapir <stavsap@gmail.com>
2023-06-27 15:48:12 +0300

    Update install_linux.sh

--- DocChat/install_linux.sh
+++ DocChat/install_linux.sh
@@ -2,6 +2,7 @@ git clone https://github.com/stavsap/LangChain-Playground.git
 cd LangChain-Playground/DocChat
 python3 -m venv venv
 source venv/bin/activate
+python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python -m pip install -r requirements.txt
 chmod +x run_linux.sh
 ./run_linux.sh
